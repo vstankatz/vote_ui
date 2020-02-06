0ce3c95b11bbf503d2bf2da43c3e63ee5c4b58e3
jonathansalfen <jonathan.salfen@gmail.com>
2020-02-06 13:32:40 -0800

    style stuff

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,5 +1,11 @@
 class InformationController < ApplicationController
+
   def index
+    @state_info = HTTParty.get('http://localhost:4567/states/57')
+    # @body = JSON.parse(@state_info.body)
     render :index
   end
+
+  # def state
+  # end
 end
