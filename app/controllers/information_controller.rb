eba35950d04c63a07878d2860073213b63a006cb
Student <example@example.com>
2020-02-06 14:13:44 -0800

    fix state location

--- app/controllers/information_controller.rb
+++ app/controllers/information_controller.rb
@@ -1,11 +1,11 @@
 class InformationController < ApplicationController
 
   def index
-    @state_info = HTTParty.get('http://localhost:4567/states/57')
-    # @body = JSON.parse(@state_info.body)
     render :index
   end
 
-  # def state
-  # end
+  def show
+    @state_info = HTTParty.get('http://localhost:4567/states/130')
+    render :show
+  end
 end
