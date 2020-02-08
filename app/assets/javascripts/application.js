3d88fad91e3435c53c34474a0009ac7799388352
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-02-07 23:33:48 -0800

    Add js for modal.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,23 @@
   //= require bootstrap-sprockets
 
   $(document).ready(function (e) {
-    // e.preventDefalt();
+    var modal = document.getElementById('modal');
+    var modalBtn = document.getElementById('modal-btn');
+    var close = document.getElementsByClassName('close');
+
+    modalBtn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    close.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
 
     $("path, circle").hover(function(e) {
       $('#info-box').css('display','block');
